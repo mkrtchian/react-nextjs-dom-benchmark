4ef1878db26d4a0f25f311606bf78e635e77c7a0
Roman Mkrtchian <mkrtchia@gmail.com>
2021-03-05 19:41:04 +0100

    maint: add dependabot updates monthly (#2)

--- next/next.config.js
+++ next/next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   trailingSlash: true,
-  assetPrefix: isProd ? '/react-nextjs-dom-benchmark/next' : '',
-  basePath: isProd ? '/react-nextjs-dom-benchmark/next' : '',
-}
+  assetPrefix: isProd ? "/react-nextjs-dom-benchmark/next" : "",
+  basePath: isProd ? "/react-nextjs-dom-benchmark/next" : "",
+};
